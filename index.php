9fa841922785b1ceb9d911d02728f0295a897ccb
Andres <andresspak@gmail.com>
2019-10-24 03:11:40 +0300

    init

---
+++ index.php
@@ -0,0 +1,185 @@
+<?php
+
+class CurlHandler
+{
+    protected $host;
+    protected $queue = [];
+
+    /**
+     * CurlHandler constructor.
+     * @param string $host
+     */
+    public function __construct($host)
+    {
+        $this->host = $host;
+    }
+
+    /**
+     * @param string $path
+     * @param array $headers
+     * @param array $params
+     * @return array
+     * @throws Exception
+     */
+    public function get($path = '', $headers = [], $params = [])
+    {
+        $path .= (strpos('?', $path) === false ? '?' : '&') . http_build_query($params);;
+        return $this->request($this->host . $path, 'GET', $headers, $params);
+    }
+
+    /**
+     * @param string $path
+     * @param array $headers
+     * @param array $params
+     * @return array
+     * @throws Exception
+     */
+    public function post($path = '', $headers = [], $params = [])
+    {
+        return $this->request($this->host . $path, 'POST', $headers, $params);
+    }
+
+    /**
+     * @param string $path
+     * @param array $headers
+     * @param array $params
+     * @return array
+     * @throws Exception
+     */
+    public function put($path = '', $headers = [], $params = [])
+    {
+        return $this->request($this->host . $path, 'PUT', $headers, $params);
+    }
+
+    /**
+     * @param string $path
+     * @param array $headers
+     * @param array $params
+     * @return array
+     * @throws Exception
+     */
+    public function patch($path = '', $headers = [], $params = [])
+    {
+        return $this->request($this->host . $path, 'PATCH', $headers, $params);
+    }
+
+    public function queue($path, $method, $headers, $params)
+    {
+        $this->queue[] = $this->initialize($path, $method, $headers, $params);
+    }
+
+    /**
+     * @return array
+     */
+    public function run()
+    {
+        $cmh = curl_multi_init();
+
+        foreach ($this->queue as $ch) {
+            curl_multi_add_handle($cmh, $ch);
+        }
+
+        $active = null;
+        do {
+            curl_multi_exec($cmh, $active);
+        } while ($active);
+
+        foreach ($this->queue as $ch) {
+            curl_multi_remove_handle($cmh, $ch);
+        }
+
+        curl_multi_close($cmh);
+
+        return array_map(function ($ch) {
+            $response = curl_multi_getcontent($ch);
+            $responseHeaderSize = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+            $responseHeaders = substr($response, 0, $responseHeaderSize);
+            $responseBody = substr($response, $responseHeaderSize);
+
+            return [
+                'headers' => $this->formatResponseHeaders($responseHeaders),
+                'data' => json_decode($responseBody),
+            ];
+        }, $this->queue);
+    }
+
+    /**
+     * @param $uri string
+     * @param $method string
+     * @param $headers array
+     * @param $params array
+     * @return array
+     * @throws Exception
+     */
+    private function request($uri, $method, $headers, $params)
+    {
+        $ch = $this->initialize($uri, $method, $headers, $params);
+
+        $response = curl_exec($ch);
+        $responseHeaderSize = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+        $responseHeaders = substr($response, 0, $responseHeaderSize);
+        $responseBody = substr($response, $responseHeaderSize);
+
+        if (curl_errno($ch)) {
+            throw new Exception(curl_error($ch));
+        }
+
+        curl_close($ch);
+
+        return [
+            'headers' => $this->formatResponseHeaders($responseHeaders),
+            'data' => json_decode($responseBody),
+        ];
+    }
+
+    /**
+     * @param $responseHeaders
+     * @return array
+     */
+    private function formatResponseHeaders($responseHeaders)
+    {
+        $headers = [];
+
+        $requests = substr($responseHeaders, 0, strpos($responseHeaders, "\r\n\r\n"));
+        foreach (explode("\r\n", $requests) as $i => $row) {
+            if ($i === 0) {
+                $headers['http_code'] = $row;
+            } else {
+                list ($key, $value) = explode(': ', $row);
+                $headers[$key] = $value;
+            }
+        }
+
+        return $headers;
+    }
+
+    /**
+     * @param string $uri
+     * @param string $method
+     * @param array $headers
+     * @param array $params
+     * @return false|resource
+     */
+    private function initialize($uri, $method, $headers, $params)
+    {
+        $ch = curl_init($uri);
+
+        curl_setopt($ch, CURLOPT_FAILONERROR, 1);
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+
+        if ($method === 'GET') {
+            curl_setopt($ch, CURLOPT_HTTPGET, 1);
+        } elseif ($method === 'POST') {
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+        } elseif ($method === 'PUT' || $method === 'PATCH') {
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+        }
+
+        return $ch;
+    }
+}
---
+++ readme.md
@@ -0,0 +1,46 @@
+# PHP cURL request handler
+
+Lightweight php cURL request handler.
+
+## Methods
+### Single method requests
+
+```php
+<?php
+$curlHandler = new CurlHandler('https://example.com'); 
+
+$curlHandler->get($path, $headers, $params);
+$curlHandler->post($path, $headers, $params);
+$curlHandler->put($path, $headers, $params);
+$curlHandler->patch($path, $headers, $params);
+```
+
+### Queued request
+Queues requests and runs them asynchronously
+
+```php
+<?php
+$curlHandler = new CurlHandler('https://example.com'); 
+
+$curlHandler->queue($path, $method, $headers, $params);
+$curlHandler->run();
+```
+
+## Example
+
+```php
+<?php
+
+$headers = [
+    'Content-Type: application/json',
+];
+
+$params = [];
+
+$curlHandler = new CurlHandler('https://example.com'); 
+
+list(
+    $responseHeaders,   // response headers
+    $responseData,      // response content
+) = $curlHandler->get('/', $headers, $params);
+```
